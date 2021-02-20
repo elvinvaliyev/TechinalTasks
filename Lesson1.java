ca289f88e208b2b529e368c8ab301ea632824004
elvinvaliyev <55504003+elvinvaliyev@users.noreply.github.com>
2021-02-20 14:02:44 +0400

    Add files via upload
    1-ci mühazirəyə aid ev tapşırıqlar

---
+++ Lesson1.java
@@ -0,0 +1,122 @@
+//1-ci mühazirəyə aid ev tapşırıqlar
+
+package lesson1;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        
+// 1. 1-dən 100-dən ədədləri çap edə
+
+//        for(int i=1;i<=100;i++){
+//        System.out.println(i);}
+
+
+
+// 2. Bir ədədin digər ikisi arasında yerləşib-yerləşmədiyini müəyyən edə
+
+//Scanner scan = new Scanner(System.in);
+//int a =scan.nextInt();
+//int b =scan.nextInt();
+//int c =scan.nextInt();
+// if((b>a && b<c)||(b>c && b<a)){
+//    System.out.println("good");}else{
+// System.out.println("NOT GOOD");}
+
+
+
+//3. Üç ədədin maksimumunu tapan
+
+//Scanner scan = new Scanner(System.in);
+//int a =scan.nextInt();
+//int b =scan.nextInt();
+//int c =scan.nextInt();
+//if((a>b && a>c)){
+//    System.out.println(a);}
+//else
+//    if((b>c && b>a)){
+//    System.out.println(b);}
+//else {
+//   System.out.println(c);}
+//Scanner scan =new Scanner(System.in);
+//int n=scan.nextInt();
+//int max=0;
+//int arr[] =new int[n];
+//
+//for(int i=0;i<n;i++){
+//arr[i]=scan.nextInt();}
+//
+//for(int i=0;i<n;i++){
+//    if(arr[i]>max){
+//    max=arr[i];}
+//}
+//        System.out.println(max);
+//        
+
+
+
+//4. 1-dən 100-dək cüt ədədləri çap edən
+
+//        for(int i=2;i<=100;i=i+2){
+//            System.out.println(i);}
+//        
+
+
+
+//5. Ədədin sadə ədəd olub olmamasını müəyyən edən
+
+//Scanner scan = new Scanner(System.in);
+//int n =scan.nextInt();
+//int S=0;
+//for(int i=2;i<=n-1;i++){
+//    if(n%i==0){
+//    S=1;
+//    }
+//}
+//if(S==1){
+//    System.out.println("Sade deyil");
+//}
+//else{
+//    System.out.println("sadedir");
+//}
+
+
+
+//6. Verilmiş mənfi olmayan tam ədədin faktorialını hesablaya
+
+//Scanner scan =new Scanner(System.in);
+//int n =scan.nextInt();
+//int s=1;
+//for(int i=n;i>1;i--){
+//    s=s*i;
+//}
+//        System.out.println(s);
+//    
+
+
+
+//7. 1-dən 100-dək tək ədədlərin cəmini hesabyan
+
+//int S=0;
+//for(int i=1;i<100;i=i+2){
+//    S=S+i;
+//}
+//        System.out.println(S);
+
+
+
+//8. Verilmiş ədədin ən böyük mərtəbə vahidini çap edən
+
+//        Scanner scan = new Scanner(System.in);
+//        int x = scan.nextInt();
+//
+//        int count = 0;
+//        while (x != 0) {
+//            x = x / 10;
+//            count++;
+//        }
+//        System.out.println((int) (Math.pow(10, count-2)));
+
+
+    }
+    
+}
